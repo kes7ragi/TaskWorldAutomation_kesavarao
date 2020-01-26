e27f116cf70593de9c2f761dfdcb131b05dedfdf
Kesava Rao <Kesava.Rao@genesys.com>
2020-01-26 23:00:37 +0530

    data common file added

---
+++ src/main/java/data/StaticData.java
@@ -0,0 +1,9 @@
+package data;
+
+public class StaticData {
+    public final static String LOGIN_USER_NAME = "test251191@gmail.com";
+    public final static String LOGIN_PASSWORD = "zxCVB@123";
+    public final static String PROJECT_NAME = "this is a sample project";
+    public final static String PROJECT_DESCRIPTION = "this is a sample description";
+    public final static String CREATE_TASKLIST_NAME = "this is a sample description";
+}
